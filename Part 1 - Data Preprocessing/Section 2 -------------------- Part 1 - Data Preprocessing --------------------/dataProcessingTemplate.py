c7486e3cd865a95cf97f33a33d7cf47554ec2d82
Rahul Kr. Singh <srahulkumar633@gmail.com>
2019-09-14 12:33:29 +0530

    Add files via upload

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/dataProcessingTemplate.py
@@ -0,0 +1,33 @@
+# importing the library 
+import  numpy as  np
+import  matplotlib as plt
+import pandas as  pd
+
+# importing  data 
+
+dataset=pd.read_csv('Data.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,3].values
+
+#Splitting  the  data  set  into traing  set  and  test set
+
+from  sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test =train_test_split(X,Y,test_size=0.2,random_state=0)
+
+
+#feature scaling
+"""
+from sklearn.preprocessing import StandardScaler
+
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+"""
+
+
+
+
+
+
+
+
